f9ef83da634aa28d81b63da957e47df7d222749f
mailbox.boyko@gmail.com <mailbox.boyko@gmail.com>
2024-09-20 10:51:25 +0300

    start

---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Главная';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+        <div class="row justify-content-center">
+            
+        </div>        
+    </div>
+</div>
